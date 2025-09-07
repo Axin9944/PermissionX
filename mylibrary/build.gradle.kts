8b0b4be80ebc8ac25fa4c9c26c9af3e99d74da22
Axin <axinnoemail@gmail.com>
2025-09-07 20:40:11 +0800

    完成PermissionX库的开发与测试工作

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.permission.app"
+    namespace = "com.permissionx.Axindev"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.permission.app"
         minSdk = 21
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,9 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(project(":mylibrary"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
